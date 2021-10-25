7049bea5bef8f748d1491a844501a8cc43e46095
Kunalk3 <41562231+kunalk3@users.noreply.github.com>
2021-10-25 19:31:35 +0530

    Update main.py

--- NSE_data_scraper/main.py
+++ NSE_data_scraper/main.py
@@ -6,7 +6,7 @@ class NSEIndia:
     def __init__(self):
         self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'}
         self.session = requests.Session()
-        self.session.get('http://nseindia.com', headers=self.headers)
+        self.session.get('http://nseindia.com', headers=self.headers) 
 
     def NsePreMarketData(self):
         pre_market_keys = {'NIFTY 50': 'NIFTY', 'NIFTY BANK': 'BANKNIFTY', 'EMERGE': 'SME', 'SECURITIES IN F&O': 'FO', 
